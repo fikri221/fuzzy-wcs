d86945aa36f452d125b8cabf4ed154cab4c1f12a
Fikri Lazuardi <fikrim69@gmail.com>
2021-04-01 16:02:00 +0700

    halaman header terpisah

--- layouts/header.php
+++ layouts/header.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Fuzzy - Dashboard</title>
+    <title>Fuzzy - <?= $title ?></title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
