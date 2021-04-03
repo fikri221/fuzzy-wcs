a383f2cc7d0b61a758d534123df24f9e8b556b43
Fikri Lazuardi <fikrim69@gmail.com>
2021-04-03 22:57:45 +0700

    update class suhu dan kelembapan

--- fungsi_perhitungan.php
+++ fungsi_perhitungan.php
@@ -6,72 +6,65 @@ class Suhu
     public $medB_suhu;
     public $medC_suhu;
     public $max_suhu;
-
-    function __construct(
-        $min_suhu,
-        $medA_suhu,
-        $medB_suhu,
-        $medC_suhu,
-        $max_suhu
-    ) {
-        $this->$min_suhu = $min_suhu;
-        $this->$medA_suhu = $medA_suhu;
-        $this->$medB_suhu = $medB_suhu;
-        $this->$medC_suhu = $medC_suhu;
-        $this->$max_suhu = $max_suhu;
+    
+    function __construct($min_suhu, $medA_suhu, $medB_suhu, $medC_suhu, $max_suhu)
+    {
+        $this->min_suhu = $min_suhu;
+        $this->medA_suhu = $medA_suhu;
+        $this->medB_suhu = $medB_suhu;
+        $this->medC_suhu = $medC_suhu;
+        $this->max_suhu = $max_suhu;
     }
 }
 
-class Ketinggian_Air
+class Kelembapan
 {
-    public $min_tinggi;
-    public $medA_tinggi;
-    public $medB_tinggi;
-    public $medC_tinggi;
-    public $max_tinggi;
-
-    function __construct(
-        $min_tinggi,
-        $medA_tinggi,
-        $medB_tinggi,
-        $medC_tinggi,
-        $max_tinggi
-    ) {
-        $this->$min_tinggi = $min_tinggi;
-        $this->$medA_tinggi = $medA_tinggi;
-        $this->$medB_tinggi = $medB_tinggi;
-        $this->$medC_tinggi = $medC_tinggi;
-        $this->$max_tinggi = $max_tinggi;
+    public $min_kelembapan;
+    public $medA_kelembapan;
+    public $medB_kelembapan;
+    public $medC_kelembapan;
+    public $max_kelembapan;
+    
+    function __construct($min_kelembapan, $medA_kelembapan, $medB_kelembapan, $medC_kelembapan, $max_kelembapan)
+    {
+        $this->min_kelembapan = $min_kelembapan;
+        $this->medA_kelembapan = $medA_kelembapan;
+        $this->medB_kelembapan = $medB_kelembapan;
+        $this->medC_kelembapan = $medC_kelembapan;
+        $this->max_kelembapan = $max_kelembapan;
     }
 }
 
 class Perhitungan
 {
-    function hitung($nilai_suhu, $nilai_ketinggianAir)
+    function hitung($nilai_suhu, $nilai_kelembapan)
     {
-        // Proses Inisiasi
-        $suhu = new Suhu(10, 20, 30, 35, 40);
-        $tinggi_air = new Ketinggian_Air(1, 5, 8, 10, 15);
+        $min_suhu = 10;
+        $medA_suhu = 20;
+        $medB_suhu = 30;
+        $medC_suhu = 35;
+        $max_suhu = 40;
 
-        $min_suhu = $suhu->min_suhu;
-        $medA_suhu = $suhu->medA_suhu;
-        $medB_suhu = $suhu->medB_suhu;
-        $medC_suhu = $suhu->medC_suhu;
-        $max_suhu = $suhu->max_suhu;
+        $min_kelembapan = 20;
+        $medA_kelembapan = 35;
+        $medB_kelembapan = 50;
+        $medC_kelembapan = 65;
+        $max_kelembapan = 80;
 
-        $min_tinggi_air = $tinggi_air->min_tinggi;
-        $medA_tinggi_air = $tinggi_air->medA_tinggi;
-        $medB_tinggi_air = $tinggi_air->medB_tinggi;
-        $medC_tinggi_air = $tinggi_air->medC_tinggi;
-        $max_tinggi_air = $tinggi_air->max_tinggi;
-
-        $suhu['rendah'] = $this->rendah($nilai_suhu, $min_suhu, $max_suhu);
+        $suhu['rendah'] = $this->rendah($nilai_suhu, $min_suhu, $medB_suhu);
         $suhu['sedang'] = $this->sedang($nilai_suhu, $medA_suhu, $medB_suhu, $medC_suhu);
-        $suhu['tinggi'] = $this->tinggi($nilai_suhu, $min_suhu, $max_suhu);
+        $suhu['tinggi'] = $this->tinggi($nilai_suhu, $medB_suhu, $max_suhu);
 
-        $ketinggianAir['rendah'] = $this->rendah($nilai_ketinggianAir, $min_tinggi_air, $max_tinggi_air);
-        $ketinggianAir['sedang'] = $this->sedang($nilai_ketinggianAir, $medA_tinggi_air, $medB_tinggi_air, $medC_tinggi_air);
-        $ketinggianAir['tinggi'] = $this->tinggi($nilai_ketinggianAir, $min_tinggi_air, $max_tinggi_air);
+        $kelembapan['rendah'] = $this->rendah($nilai_kelembapan, $min_kelembapan, $max_kelembapan);
+        $kelembapan['sedang'] = $this->sedang($nilai_kelembapan, $medA_kelembapan, $medB_kelembapan, $medC_kelembapan);
+        $kelembapan['tinggi'] = $this->tinggi($nilai_kelembapan, $min_kelembapan, $max_kelembapan);
+
+        $hasil = array(
+            "suhu" => $suhu,
+            "kelembapan" => $kelembapan,
+        );
+        $hasil;
+        return $hasil;
     }
 
     function rendah($nilai, $min, $max)
